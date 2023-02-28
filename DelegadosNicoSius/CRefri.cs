841d2ab2d6ba7dd1ea48c72f0ea1f07f7ea1340d
juansilva <juandsr@hotmail.com>
2023-02-28 18:00:12 -0500

    referencias delegados simple

---
+++ DelegadosNicoSius/CRefri.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegadosNicoSius
+{
+    internal class CRefri
+    {
+        public delegate void DReservasBajas(int pKilos);
+        public delegate void DDescongelado(int pGrados);
+
+        private int kilosAlimentos = 0;
+        private int grados = 0;
+
+        private DReservasBajas delReservas;
+        private DDescongelado delDescongelado;
+
+        public int KilosAlimentos { get => kilosAlimentos; }
+        public int Grados { get => grados; }
+
+        public CRefri(int kilosAlimentos, int grados)
+        {
+            this.kilosAlimentos = kilosAlimentos;
+            this.grados = grados;
+        }
+
+        public void AdicionarMetodoReservas(DReservasBajas pMetodo)
+        {
+            delReservas = pMetodo;
+        }
+
+        public void AdicionaMetodoDescongelado(DDescongelado pMetodo)
+        {
+            delDescongelado = pMetodo;
+        }
+        public void Trabajar(int pConsumo)
+        {
+            kilosAlimentos -= pConsumo;
+
+            grados += 1;
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine($"{kilosAlimentos} kilos, {grados} grados");
+
+            if (kilosAlimentos < 10)
+            {
+                delReservas(kilosAlimentos);
+            }
+
+            if (grados > 0)
+            {
+                delDescongelado(grados);
+            }
+        }
+    }
+}
---
+++ DelegadosNicoSius/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegadosNicoSius
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            CRefri miRefri = new CRefri(70, -20);
+
+            miRefri.AdicionarMetodoReservas(new CRefri.DReservasBajas(InformeKilos));
+            miRefri.AdicionaMetodoDescongelado(new CRefri.DDescongelado(InformeKilos));
+
+            while (miRefri.KilosAlimentos > 0)
+            {
+                miRefri.Trabajar(rnd.Next(1,5));
+            }
+        }
+
+        public static void InformeKilos(int pKilos)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("---> Reservas bajas de alimentos, estoy anivel de main");
+            Console.WriteLine($"---> quedan {pKilos}");
+        }
+
+        public static void InformeGrados(int pGrados)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("---> Reservas bajas de alimentos, estoy anivel de main");
+            Console.WriteLine($"---> quedan {pGrados}");
+        }
+    }
+}
