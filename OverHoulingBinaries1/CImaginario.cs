e3a867a0172c39d30f0e1427c2aaed48611b1bc8
juansilva <juandsr@hotmail.com>
2023-03-02 19:19:37 -0500

    Overhouling (<,>) operators

--- OverHoulingBinaries1/CImaginario.cs
+++ OverHoulingBinaries1/CImaginario.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace OverHoulingBinaries1
 {
-    internal class CImaginario
+    internal class CImaginario : IComparable
     {
         private double entero;
         private double imaginario;
@@ -112,6 +112,44 @@ namespace OverHoulingBinaries1
         public static bool operator !=(CImaginario i1, CImaginario i2)
         {
             return !(i1.Equals(i2));
+        }
+
+        public double magnitud()
+        {
+            double m = 0;
+
+            m = Math.Sqrt((entero * entero) + (imaginario * imaginario));
+
+            return m;
+        }
+        public int CompareTo(object obj)
+        {
+            if(obj is CImaginario)
+            {
+                CImaginario temp = (CImaginario) obj;
+
+                if (magnitud()> temp.magnitud())
+                    return 1;
+                if (magnitud()< temp.magnitud())
+                    return -1;
+            }
+            return 0;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static bool operator >(CImaginario i1, CImaginario i2)
+        {
+            if (i1.CompareTo(i2) < 0) return false;
+            else return true;
+        }
+        public static bool operator <(CImaginario i1, CImaginario i2)
+        {
+            if (i1.CompareTo(i2) > 0) return false;
+            else return true;
         }
     }
 }
